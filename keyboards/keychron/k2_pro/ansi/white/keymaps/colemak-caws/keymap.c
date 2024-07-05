3cb402accb45f5dcbc4700626763b7a03be8751d
2goodAP <45424904+2goodAP@users.noreply.github.com>
2024-07-05 07:34:36 +0545

    Change symbol keymaps for k2_pro rgb and white

--- keyboards/keychron/k2_pro/ansi/white/keymaps/colemak-caws/keymap.c
+++ keyboards/keychron/k2_pro/ansi/white/keymaps/colemak-caws/keymap.c
@@ -27,8 +27,8 @@ enum layers{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC_BASE] = LAYOUT_ansi_84(
      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,   KC_DEL,   KC_SNAP,  BL_STEP,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_EQL,   KC_7,     KC_8,     KC_9,       KC_0,     KC_MINS,  KC_CAPS,            KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_LBRC,  KC_J,     KC_L,     KC_U,     KC_Y,       KC_SCLN,  KC_QUOT,  KC_BSLS,            KC_PGDN,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_BSLS,  KC_7,     KC_8,     KC_9,       KC_0,     KC_EQL,   KC_CAPS,            KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_LBRC,  KC_J,     KC_L,     KC_U,     KC_Y,       KC_QUOT,  KC_MINS,  KC_SCLN,            KC_PGDN,
      KC_BSPC,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_RBRC,  KC_M,     KC_N,     KC_E,     KC_I,       KC_O,               KC_ENT,             KC_HOME,
      KC_LSFT,            KC_X,     KC_C,     KC_D,     KC_V,     KC_Z,     KC_SLSH,  KC_K,     KC_H,     KC_COMM,    KC_DOT,             KC_RSFT,  KC_UP,    KC_END,
      KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 MO(MAC_FN), KC_RCMMD, KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [WIN_BASE] = LAYOUT_ansi_84(
      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,   KC_DEL,   KC_PSCR,  BL_STEP,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_EQL,   KC_7,     KC_8,     KC_9,       KC_0,     KC_MINS,  KC_CAPS,            KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_LBRC,  KC_J,     KC_L,     KC_U,     KC_Y,       KC_SCLN,  KC_QUOT,  KC_BSLS,            KC_PGDN,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_BSLS,  KC_7,     KC_8,     KC_9,       KC_0,     KC_EQL,   KC_CAPS,            KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_LBRC,  KC_J,     KC_L,     KC_U,     KC_Y,       KC_QUOT,  KC_MINS,  KC_SCLN,            KC_PGDN,
      KC_BSPC,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_RBRC,  KC_M,     KC_N,     KC_E,     KC_I,       KC_O,               KC_ENT,             KC_HOME,
      KC_LSFT,            KC_X,     KC_C,     KC_D,     KC_V,     KC_Z,     KC_SLSH,  KC_K,     KC_H,     KC_COMM,    KC_DOT,             KC_RSFT,  KC_UP,    KC_END,
      KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 MO(WIN_FN), KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
